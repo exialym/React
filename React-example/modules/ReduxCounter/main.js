b9e4ceea7f8ceb143362a73988063ac25e610091
exialym <exialym@icloud.com>
2016-11-04 21:22:36 +0800

    counterReducer

--- React-example/modules/ReduxCounter/main.js
+++ React-example/modules/ReduxCounter/main.js
@@ -1,4 +1,14 @@
 import React from 'react'
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return state + 1;
+    case 'SUB':
+      return state - 1;
+    default:
+      return state;
+  }
+}
 export default React.createClass({
   render() {
     return <div>Counter</div>
