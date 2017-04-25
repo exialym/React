e47d89181c4fc67aa090a392aaaf57e33eee4efa
exialym <exialym@icloud.com>
2017-04-25 23:12:39 +0800

    begin learning React deeply

---
+++ React-learn/index.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom'
+import Tabs from "./modules/Tabs";
+
+render((
+  <Tabs/>
+), document.getElementById('app'));
+
+
---
+++ React-learn/modules/Tabs.js
@@ -0,0 +1,14 @@
+/**
+ * Created by exialym on 2017/4/25.
+ */
+import React, { Component, PropTypes } from 'react';
+class Tabs extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // ...
+  render() {
+    return <div className="ui-tabs">ba</div>;
+  }
+}
+export default Tabs;
