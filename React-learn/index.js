73dc7f428fdd56e716fc0fd26d1b4fbb1c5a1ba7
exialym <exialym@icloud.com>
2017-04-26 22:54:34 +0800

    clean project

--- React-learn/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component, PropTypes } from 'react';
-import { render } from 'react-dom'
-import Tabs from "./modules/Tabs";
-
-render((
-  <Tabs/>
-), document.getElementById('app'));
-
-
